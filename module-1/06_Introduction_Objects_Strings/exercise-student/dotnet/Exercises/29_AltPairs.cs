2c678d44b19982cc2fd06b3763bc5721c3282e59
maddy bovard <m.bovard55@gmail.com>
2021-01-19 22:50:40 -0500

    submitting object strings

--- module-1/06_Introduction_Objects_Strings/exercise-student/dotnet/Exercises/29_AltPairs.cs
+++ module-1/06_Introduction_Objects_Strings/exercise-student/dotnet/Exercises/29_AltPairs.cs
@@ -10,7 +10,54 @@
         */
         public string AltPairs(string str)
         {
-            return null;
+            string firstTwo = "";
+            string middleTwo = "";
+            string lastTwo = "";
+            string lastOne = "";
+            string middleOne = "";
+
+            if (str.Length == 0)
+            {
+                return "";
+            }
+            if (str.Length == 1)
+            {
+                return str.Substring(0, 1);
+            }
+            if (str.Length == 5)
+            {
+                firstTwo = str.Substring(0, 2);
+                middleOne = str.Substring(4, 1);
+                return firstTwo + middleOne;
+            }
+            if (str.Length == 9)
+            {
+                firstTwo = str.Substring(0, 2);
+                middleTwo = str.Substring(4, 2);
+                lastOne = str.Substring(8, 1);
+                return firstTwo + middleTwo + lastOne;
+            }
+            if (str.Length > 1 && str.Length < 6)
+            {
+                firstTwo = str.Substring(0, 2);
+                return firstTwo;
+            }
+
+            if (str.Length > 5 && str.Length < 10)
+            {
+                firstTwo = str.Substring(0, 2);
+                middleTwo = str.Substring(4, 2);
+                return firstTwo + middleTwo;
+            }
+            
+            else
+            {
+                firstTwo = str.Substring(0, 2);
+                middleTwo = str.Substring(4, 2);
+                lastTwo = str.Substring(8, 2);
+                return firstTwo + middleTwo + lastTwo;
+            }
+          
         }
     }
 }
