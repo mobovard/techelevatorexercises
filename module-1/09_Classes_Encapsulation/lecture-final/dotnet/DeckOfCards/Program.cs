8797418acc8604f419afced65ccfa651b56656bd
joe riggs <joe.riggs@techelevator.com>
2021-01-21 12:09:44 -0500

    exercises

---
+++ module-1/09_Classes_Encapsulation/lecture-final/dotnet/DeckOfCards/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //class for deck
+            //collection of cards
+            //deal()
+            //shuffle()
+            Card aceOfSpades = new Card("Spades", 1);
+            string faceValue = aceOfSpades.FaceValue;
+            
+            //static property
+            string clubs = Card.suits[0];
+
+        }
+    }
+}
