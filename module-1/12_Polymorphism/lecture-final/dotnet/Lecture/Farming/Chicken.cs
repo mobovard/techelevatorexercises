41d1db46850f645578c4d279cbff0742c153e786
joe riggs <joe.riggs@techelevator.com>
2021-01-26 12:01:25 -0500

    exercise

---
+++ module-1/12_Polymorphism/lecture-final/dotnet/Lecture/Farming/Chicken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lecture.Farming
+{
+    public class Chicken : FarmAnimal, IEggProducer
+    {
+        public Chicken() : base("Chicken")
+        {
+        }
+
+        public void LayEgg()
+        {
+            Console.WriteLine("Chicken laid an egg!");
+        }
+
+        public override string MakeSound()
+        {
+            return "cluck";
+        }
+    }
+}
