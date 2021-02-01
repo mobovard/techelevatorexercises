eadd412d8f9d1fc1705c88f4ee80197e9b3b6778
joe riggs <joe.riggs@techelevator.com>
2021-02-01 08:53:11 -0500

    day 16 lecture

---
+++ module-1/16_FileIO_Reading_in/lecture-student/dotnet/ExceptionHandling/Program.cs
@@ -0,0 +1,14 @@
+﻿using ExceptionHandling.Exceptions;
+using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+       
+    }
+}
