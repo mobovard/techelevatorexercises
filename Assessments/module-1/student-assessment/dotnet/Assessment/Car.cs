53d02c0d6f8b7c4d6768d11c206dec0bb59de2ed
maddy bovard <m.bovard55@gmail.com>
2021-02-12 10:19:54 -0500

    submitting assessment

---
+++ Assessments/module-1/student-assessment/dotnet/Assessment/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assessment
+{
+    class Car
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public bool IsClassicCar { get; set; }
+        public int Age
+        {
+            get
+            {
+                int age = 2021 - Year;
+                return age;
+            }
+            set
+            {
+            }
+        }
+
+        public Car(int year, string make, bool isClassicCar)
+        {
+            Year = year;
+            Make = make;
+            IsClassicCar = IsClassicCar;
+        }
+
+
+    }
+}
--- Assessments/module-1/student-assessment/dotnet/Assessment/Program.cs
+++ Assessments/module-1/student-assessment/dotnet/Assessment/Program.cs
@@ -1,13 +1,61 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Assessment
 {
     class Program
+
     {
-        static void Main(string[] args)
+        public void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
-        }
+            string filePath = @"C:\Users\Student\workspace\mbovard-c\Assessments\module-1\student-assessment\dotnet\Assessment\Data\CarInput.txt";
+
+          //  List<Car> List(string filePath)
+            {
+                
+                try
+                {
+                    using (StreamReader sr = new StreamReader(filePath))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            string[] carArray = line.Split(",");
+
+                            int year = Convert.ToInt32(carArray[0]);
+                            string model = carArray[1];
+                            bool isClassicCar = Convert.ToBoolean(carArray[2]);
+
+                            Car car = new Car(year, model, isClassicCar);
+
+                            List<Car> carList = new List<Car>();
+                            carList.Add(car);
+
+                          
+                           
+                        }
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                return List<Car>;
+            }
+
+
+           // public override string ToString()
+        //{
+            
+          //  return $"CAR - {year} - {make}";
+        //}
+
+    }
+
+
     }
 }
